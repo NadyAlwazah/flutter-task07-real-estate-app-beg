2523025161169179a8563ed1cf81239ad8f2a7b3
Nady Alwazah <nadysy.7@gmail.com>
2026-06-17 20:40:21 +0300

    feat(auth/login): add AuthHeader to LoginViewBody

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task07_real_estate_app_beg/features/auth/presentation/views/widgets/auth_header.dart';
 import 'package:flutter_task07_real_estate_app_beg/features/auth/presentation/views/widgets/signup_avatar.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -10,7 +11,17 @@ class LoginViewBody extends StatelessWidget {
       padding: EdgeInsets.all(16.0),
       child: SingleChildScrollView(
         child: Center(
-          child: Column(children: [SizedBox(height: 40), AppAvatar()]),
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              AppAvatar(),
+              SizedBox(height: 20),
+              AuthHeader(
+                title: "Login Account",
+                subtitle: "Login to continue using the app",
+              ),
+            ],
+          ),
         ),
       ),
     );
