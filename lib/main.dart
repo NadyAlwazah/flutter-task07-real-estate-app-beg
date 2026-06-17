9984e10ee37d41a7f13922e5028c3ba2c4b604a5
Nady Alwazah <nadysy.7@gmail.com>
2026-06-17 12:49:43 +0300

    feat(firebase): integrate Firebase into the project and complete initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_task07_real_estate_app_beg/features/auth/presentation/views/signup_view.dart';
+import 'package:flutter_task07_real_estate_app_beg/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const RealEstateApp());
 }
 
